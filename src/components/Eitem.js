a835533d3b82e60ea3c26e600cf4942981d59428
Nikhil Pujar <nikhilpujar23@gmail.com>
2022-06-16 15:14:34 +0530

    confirm

---
+++ src/components/Eitem.js
@@ -0,0 +1,22 @@
+
+// function Eitem(){
+//     return 
+    
+//     <div className="display-flex">
+// <div>Date</div>
+//     <h1>Title</h1>
+//     <div>Amuont</div>
+//     </div>
+// }
+
+
+
+export default function Eitem() {
+  return (
+    <div >
+      <h1 className="text-yellow-400 font-sm">This is text</h1>
+    </div>
+  )
+}
+
+
